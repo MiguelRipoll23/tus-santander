24961dbe9a3a9acbbacc2a6586e66a31a5e27f04
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2023-06-09 22:36:44 +0200

    Improve map view (#159)
    * Improve map view
    
    * Improve marker color
    
    * Improve font styles
    
    * Improve marker icon
    
    * Improve text shadow
    
    * Improve text shadow
    
    * Increment markers limit to 20
    
    * Revert "Increment markers limit to 20"
    
    This reverts commit 96f10d3091220468f5b6c8baf6823e8b241cdb9d.

--- src/views/map/MapView.js
+++ src/views/map/MapView.js
@@ -105,6 +105,18 @@ const MapView = (props) => {
   const mapOptions = {
     fullscreenControl: false,
     disableDefaultUI: true,
+    styles: [
+      {
+        featureType: "poi.business",
+        elementType: "labels",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "transit.station.bus",
+        elementType: "labels.icon",
+        stylers: [{ visibility: "off" }],
+      },
+    ],
   };
 
   const handleOnLoad = (loadedMap) => {
@@ -141,9 +153,10 @@ const MapView = (props) => {
           <Marker
             key={i}
             label={{
-              color: "#1da1f2",
               text: marker.text,
-              fontSize: "14px",
+              fontSize: "20px",
+              fontWeight: "bold",
+              className: "marker",
             }}
             position={marker.position}
             icon={{
