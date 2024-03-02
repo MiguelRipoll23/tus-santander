4045bf6b11a7c47b31294ac66c14d4499e85f879
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2024-03-02 14:45:00 +0100

    try new maker icon

--- src/components/map/ClosestMarkers.js
+++ src/components/map/ClosestMarkers.js
@@ -2,7 +2,7 @@ import { AdvancedMarker } from "@vis.gl/react-google-maps";
 import { useView } from "../../contexts/ViewContext.js";
 import { VIEW_ID_ESTIMATIONS_STOP } from "../../constants/ViewConstants.js";
 
-import MarkerMin from "../../assets/marker-min.png";
+import MarkerMin from "../../assets/marker-v2-min.png";
 
 const ClosestMarkers = (props) => {
   const { markers } = props;
