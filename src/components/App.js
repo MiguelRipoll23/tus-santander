2fae535e7b46e96958d5299428157389b13ad8fb
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2024-04-15 16:49:40 +0200

    Show FAB always in view

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
 import { ViewProvider } from "../providers/ViewProvider.js";
 import View from "./View.js";
 
-window.standalone =
-  navigator.standalone ||
-  window.matchMedia("(display-mode: standalone)").matches;
-
 const App = () => {
   return (
     <ViewProvider>
