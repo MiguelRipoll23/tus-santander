a99d057032b7e502ecddf41c4967507ab4711730
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-02-02 16:35:28 +0100

    Remove console log for API_HOST in ApiConstants

--- src/utils/ApiConstants.jsx
+++ src/utils/ApiConstants.jsx
@@ -2,4 +2,3 @@ export const GOOGLE_MAPS_KEY = import.meta.env.VITE_APP_GOOGLE_MAPS_KEY;
 export const API_HOST = import.meta.env.VITE_APP_API_HOST;
 export const API_PATH_JSON_ESTIMATIONS = "/v4/estimations";
 export const API_PATH_JSON_ROUTE = "/v4/route";
-console.log(API_HOST);
