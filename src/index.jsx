8a74212e5e2bffe2b0f182e6df4e5acd05a85147
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-02-02 17:19:13 +0100

    Update PWA configuration and remove legacy service worker files

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./components/App.jsx";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration.jsx";
 
 const container = document.querySelector("#app");
 const root = createRoot(container);
@@ -13,21 +12,3 @@ root.render(
     <App />
   </StrictMode>
 );
-
-const config = {
-  onUpdate: (registration) => {
-    const waitingServiceWorker = registration.waiting;
-
-    if (waitingServiceWorker) {
-      waitingServiceWorker.addEventListener("statechange", (event) => {
-        if (event.target.state === "activated") {
-          window.location.reload();
-        }
-      });
-
-      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
-    }
-  },
-};
-
-serviceWorkerRegistration.register(config);
