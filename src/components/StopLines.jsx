b03a75f4e7645bbf9c71210a09d268b833e65151
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-05-09 18:49:21 +0200

    Refactor StopLines component for better styling
    Updated StopLines.jsx to improve readability and maintainability by refactoring props, adding dynamic styles, and using utility functions. Adjusted StopLines.module.css to support small and large button styles with CSS variables for better flexibility.

--- src/components/StopLines.jsx
+++ src/components/StopLines.jsx
@@ -1,22 +1,16 @@
-import Styles from "./StopLines.module.css";
-
+import styles from "./StopLines.module.css";
+import StyleUtils from "../utils/StyleUtils.jsx";
 import {
   getLineBackgroundColor,
   getLineTextColor,
 } from "../utils/LineUtils.jsx";
 
 const isDisabled = (label, estimations) => {
-  // Check if coming from route view
-  if (estimations === undefined) {
-    return false;
-  }
+  if (estimations === undefined) return false;
 
   for (const item of estimations) {
     const [etaLabel] = item;
-
-    if (label === etaLabel) {
-      return false;
-    }
+    if (label === etaLabel) return false;
   }
 
   return true;
@@ -27,19 +21,35 @@ const handleOnClick = (label) => {
   labelElement?.scrollIntoView({ behavior: "smooth", block: "center" });
 };
 
-const StopLines = (props) => {
+const StopLines = ({ size, list, estimations }) => {
+  const isSmall = size === "small";
+  const height = isSmall ? "28px" : "36px";
+  const paddingLeft = isSmall ? "0" : StyleUtils.MARGIN_LR;
+
   return (
-    <div className={Styles.StopLines}>
-      {props.list.map((label, i) => {
+    <div
+      className={styles.stopLines}
+      style={{
+        "--padding-left": paddingLeft,
+        "--margin-lr": StyleUtils.MARGIN_LR,
+        "--height": height,
+      }}
+    >
+      {list.map((label, i) => {
+        const isDisabledBtn = isDisabled(label, estimations);
+        const buttonClass = `${styles.stopLine} ${
+          isSmall ? styles.stopLineSmall : styles.stopLineLarge
+        }`;
+
         return (
           <button
             key={i}
-            className={Styles.StopLine}
+            className={buttonClass}
+            disabled={isDisabledBtn}
             style={{
-              backgroundColor: getLineBackgroundColor(label, "string"),
+              background: getLineBackgroundColor(label, "string"),
               color: getLineTextColor(label),
             }}
-            disabled={isDisabled(label, props.estimations)}
             onClick={() => handleOnClick(label)}
           >
             {label}
